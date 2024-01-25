26dee65257a87bab6f3d65ec86b9b71e49c4a81b
Daniel McCarthy <dmccarthy2012@hotmail.co.uk>
2024-01-25 20:24:04 +1100

    save password with 600 permissions

--- src/encryption.rs
+++ src/encryption.rs
@@ -3,9 +3,10 @@ use openssl::hash::MessageDigest;
 use openssl::pkcs5::pbkdf2_hmac;
 use openssl::rand::rand_bytes;
 use openssl::symm::{Cipher, Crypter, Mode};
-use std::fs::File;
+use std::fs::{File,Permissions};
 use std::io::{Read, Write};
 use std::path::PathBuf;
+use std::os::unix::fs::PermissionsExt;
 
 const ITERATIONS: usize = 100_000;
 const KEY_LENGTH: usize = 32; //32 bytes = 256bit which is the key length size aes_256_cbc expects
@@ -66,6 +67,8 @@ impl CryptoManager {
         file.write_all(&self.salt)?;
         file.write_all(&self.iv)?;
         file.write_all(&self.ciphertext)?;
+        let permissions = Permissions::from_mode(0o600);
+        std::fs::set_permissions(&self.filepath, permissions)?; 
         Ok(())
     }
 
